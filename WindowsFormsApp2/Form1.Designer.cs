ffe0ddd095eec7ac7fdba47add200b37c0eb9d31
kowalski.marcel <kowalski.marcel@STACJA39A-11>
2022-11-28 08:33:52 +0100

    Dodaj pliki projektów.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dodajGalaźToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dodajPodgalaźToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuńToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.źmieńToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(676, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(676, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 22);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Zamknij";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.AllowDrop = true;
+            this.treeView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.treeView1.FullRowSelect = true;
+            this.treeView1.HideSelection = false;
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(642, 408);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dodajGalaźToolStripMenuItem,
+            this.dodajPodgalaźToolStripMenuItem,
+            this.usuńToolStripMenuItem,
+            this.źmieńToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(157, 92);
+            this.contextMenuStrip1.Text = "Dodaj podgałąź";
+            // 
+            // dodajGalaźToolStripMenuItem
+            // 
+            this.dodajGalaźToolStripMenuItem.Name = "dodajGalaźToolStripMenuItem";
+            this.dodajGalaźToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.dodajGalaźToolStripMenuItem.Text = "Dodaj gałąź";
+            this.dodajGalaźToolStripMenuItem.Click += new System.EventHandler(this.dodajGalaźToolStripMenuItem_Click);
+            // 
+            // dodajPodgalaźToolStripMenuItem
+            // 
+            this.dodajPodgalaźToolStripMenuItem.Name = "dodajPodgalaźToolStripMenuItem";
+            this.dodajPodgalaźToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.dodajPodgalaźToolStripMenuItem.Text = "Dodaj podgałąź";
+            this.dodajPodgalaźToolStripMenuItem.Click += new System.EventHandler(this.dodajPodgalaźToolStripMenuItem_Click);
+            // 
+            // usuńToolStripMenuItem
+            // 
+            this.usuńToolStripMenuItem.Name = "usuńToolStripMenuItem";
+            this.usuńToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.usuńToolStripMenuItem.Text = "Usuń";
+            this.usuńToolStripMenuItem.Click += new System.EventHandler(this.usuńToolStripMenuItem_Click);
+            // 
+            // źmieńToolStripMenuItem
+            // 
+            this.źmieńToolStripMenuItem.Name = "źmieńToolStripMenuItem";
+            this.źmieńToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.źmieńToolStripMenuItem.Text = "Zmień";
+            this.źmieńToolStripMenuItem.Click += new System.EventHandler(this.źmieńToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Firma";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dodajGalaźToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dodajPodgalaźToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuńToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem źmieńToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            OdczytZPilku();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+
+        private void dodajGalaźToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 dialog = new Form2();
+            dialog.Text = "Dodawanie gałęzi";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                treeView1.Nodes.Add(dialog.nazwa);
+            }
+        }
+
+        private void dodajPodgalaźToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            Form2 dialog = new Form2();
+            dialog.Text = "Dodawanie podgałęzi";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                treeView1.SelectedNode.Nodes.Add(dialog.nazwa);
+            }
+        }
+
+        private void źmieńToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 dialog = new Form2();
+            dialog.Text = "Modyfikowanie gałęzi";
+            dialog.nazwa = treeView1.SelectedNode.Text;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                treeView1.SelectedNode.Text = dialog.nazwa;
+            }
+        }
+
+        private void usuńToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Remove(treeView1.SelectedNode);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void DodajDoListy(TreeNode node, ref List<myNode> lista) 
+        {
+            if (node == null)
+                return;
+            string r, n;
+            if (node.Parent == null) 
+            r = "brak";
+            else
+                r = node.Parent.Text;
+            n = node.Text;
+
+            lista.Add(new myNode(r, n));
+            if (node.NextNode != null)
+                DodajDoListy(node.NextNode, ref lista);
+            if (node.GetNodeCount(true) > 0)
+                DodajDoListy(node.FirstNode, ref lista);
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<myNode> lista = new List<myNode>();
+            DodajDoListy(treeView1.Nodes[0], ref lista);
+            if (lista.Count == 0)
+                return;
+
+            string text = "";
+            foreach(myNode elem in lista)
+            {
+                text += elem.nazwa + " " + elem.rodzic + "\n";
+            }
+            File.WriteAllText("firma.txt", text);
+        }
+        private void OdczytZPilku()
+        {
+            treeView1.Nodes.Clear();
+            List<myNode> lista = new List<myNode>();
+            string[] tab = File.ReadAllLines("firma.txt");
+            foreach (string elem in tab) 
+            {
+                string[] pom = elem.Split( ' ' );
+                lista.Add(new myNode(pom[1], pom[0]));
+                
+            }
+            foreach (myNode node in lista) 
+            {
+                if (node.rodzic == "brak")
+                    treeView1.Nodes.Add(new TreeNode(node.nazwa));
+                else 
+                {
+                    TreeNode rodzic = ZnajdzRodzica(treeView1.Nodes[0], node.rodzic);
+                    if (rodzic != null)
+                        rodzic.Nodes.Add(node.nazwa);
+                }
+            }
+        }
+        private TreeNode ZnajdzRodzica(TreeNode node, string rodzic)
+        {
+            TreeNode wynik = null;
+            if (node == null)
+                return null;
+            if (node.Text == rodzic)
+                return node;
+
+            if (node.NextNode != null)
+                wynik = ZnajdzRodzica(node.NextNode, rodzic);
+            if (node.GetNodeCount(true) > 0)
+                wynik = ZnajdzRodzica(node.FirstNode, rodzic);
+
+            return wynik;
+        }
+    }
+    public class myNode
+    {
+        public string rodzic;
+        public string nazwa;
+
+        public myNode(string rodzic, string nazwa)
+        {
+            this.rodzic = rodzic;
+            this.nazwa = nazwa;
+        }
+    }
+}
+
+
